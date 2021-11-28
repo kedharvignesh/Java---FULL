462e2455785cc0eebbe06461de3aa14dd2b45f87
kedharvignesh <91962033+kedharvignesh@users.noreply.github.com>
2021-11-28 17:47:11 +0530

    Add files via upload

---
+++ Week4/LoginPage/main/java/login/LoginServlet.java
@@ -0,0 +1,30 @@
+package login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if (username.equals("kedhar") && password.equals("12345")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("username", username);
+			response.sendRedirect("welcome.jsp");
+		} else {
+			response.sendRedirect("index.jsp");
+		}
+	}
+
+}
